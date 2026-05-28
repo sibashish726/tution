2567be2e65f56c3a7efd60e85e2aa40f4007d427
sibashish726 <sibashishbiswas656.com>
2026-05-28 18:20:27 +0530

    UI changes for bg and fix issues profile cards

--- scripts/team.js
+++ scripts/team.js
@@ -4,16 +4,11 @@ Vue.component('Card', {
 	template: `
     <div class="card-wrap" ref="Card">
       <div class="Card">
-        <div class="card-img-section" :style="cardBgImage">
-          <div class="card-img-gradient"></div>
-          <h3 class="team-card-title"><slot name="header"></slot></h3>
-        </div>
+        <div class="card-img-section" :style="cardBgImage"></div>
         <div class="card-bottom">
+          <h3 class="team-card-title"><slot name="header"></slot></h3>
           <p class="team-card-role"><slot name="role"></slot></p>
-          <div class="card-footer-row">
-            <div class="team-card-tags"><slot name="tags"></slot></div>
-            <div class="team-card-links"><slot name="content"></slot></div>
-          </div>
+          <div class="team-card-links"><slot name="content"></slot></div>
         </div>
       </div>
     </div>`,
